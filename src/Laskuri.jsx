7fb5727b282ff66c8ef2b07df495368408bd58e0
Simo Siren <sirensimo7@gmail.com>
2021-11-05 10:27:25 +0200

    projekti aloitettu

---
+++ src/Laskuri.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg'
+import './App.css'
+
+function Laskuri() {
+  return (
+    <div className="App">
+        <p>Tämä on laskuri</p>
+    </div>
+  )
+}
+
+export default Laskuri
