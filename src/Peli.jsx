0a6ccfa3a21fb53cc4727eb17f4ebacc8dfef54e
Simo Siren <sirensimo7@gmail.com>
2021-11-09 13:03:15 +0200

    lisätiedon piilotus myös vihreää aluetta klikkaamalla

--- src/Peli.jsx
+++ src/Peli.jsx
@@ -16,13 +16,17 @@ const Peli = (props) => {
             <h4 onClick={() => setNäytetäänkö(!näytetäänkö)}>{props.peli.nimi}</h4>
 
            {näytetäänkö &&
-           <div className="Pelitiedot">
+           <div className="Pelitiedot" onClick={() => setNäytetäänkö(!näytetäänkö)}>
+
+               <h5 style={{color: 'white'}}>{props.peli.nimi}</h5>
                 <table>
                     <thead>
                         <tr>
                             <th>Genre ID</th>
                             <th>Julkaisuvuosi</th>
                             <th>Lataukset</th>
+                            <th></th>
+                            <th></th>
                         </tr>
                     </thead>
                     <tbody>
@@ -30,6 +34,8 @@ const Peli = (props) => {
                             <td>{props.peli.genreId}</td>
                             <td>{props.peli.julkaisuvuosi}</td>
                             <td>{props.peli.lataukset}</td>
+                            <td><button>poista</button></td>
+                            <td><button>muokkaa</button></td>
                         </tr>
                     </tbody>
                     
