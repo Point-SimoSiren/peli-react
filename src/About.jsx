b4fd097585669686d54248ba7319a4b034c09f30
Simo Siren <sirensimo7@gmail.com>
2021-11-11 14:08:08 +0200

    luokka esim kuvan kanssa valmis

--- src/About.jsx
+++ src/About.jsx
@@ -4,7 +4,7 @@ import React, {Component} from 'react'
 import './App.css'
 
 class About extends Component {
-
+    // Jos tarvitaan luokkakomponentille tila pitää olla konstruktori
     constructor(props) {
         super(props)
         this.state = {
@@ -12,18 +12,21 @@ class About extends Component {
                 kuukaudenPelaaja: "Maikki1997"
         }
     }
-    
 
+    componentDidMount() {
+        console.log("componentDidMount metodista")
+    }
+    
     // Render metodi on ainoa pakollinen life-cycle metodi
     render() {
-
+        console.log("Render metodista")
         return(
             <div className="App">
             <h2>Tietoja</h2>
             <p>Olemme Suomen 44. suurin pelialan foorumi</p>
 
-            {showImage && <img src="" />}
-
+            {!this.state.showImage && <button onClick={() => this.setState({showImage: !this.state.showImage}) }>Katso kuvamme jos uskallat</button>}
+            {this.state.showImage && <img src="https://dynamicmedia.livenationinternational.com/Media/n/k/b/ec9d0aa9-a080-47bf-966a-8f44d1dbd407.jpg?auto=webp&width=1507.2" />}
 
             </div>
           )
